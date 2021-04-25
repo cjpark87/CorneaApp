abd368262eb7b5f3d10de5bcf585e7381d77ac53
Erica Chu <lhest50537@gmail.com>
2021-04-25 12:30:19 -0700

    Disable top buttons and import Placido rings images

--- Swift/AVCam/PreviewView.swift
+++ Swift/AVCam/PreviewView.swift
@@ -14,6 +14,8 @@ class PreviewView: UIView {
         guard let layer = layer as? AVCaptureVideoPreviewLayer else {
             fatalError("Expected `AVCaptureVideoPreviewLayer` type for layer. Check PreviewView.layerClass implementation.")
         }
+        //layer.frame = CGRect(x:0, y:270, width:400, height:400) // iphone Xs
+        //layer.videoGravity = .resize
         return layer
     }
     
